52c7c304b5e6cf892db64562a38838767cde02e6
realneo <neo@yoteyote.com>
2014-10-18 12:21:56 +0300

    Adds SMS functionality to Declined Process

--- process/decline_process.php
+++ process/decline_process.php
@@ -1,13 +1,23 @@
 <?php
     require_once('../lib/config/autoload.php');
     require_once('../lib/classes/Orders.php');
+    require_once('../lib/classes/Customers.php');
 
     $Orders = new Orders($db);
+    $SMS = new SMS();
+    $Customers = new Customers($db);
     
     $order_id = $_POST['id'];
     $reason = $_POST['reason'];
     $query = $Orders->update_decline($order_id, 'Declined', $reason);
-
+    
+    $order = $Orders->get_order_by_id($order_id);
+    $customer = $Customers->get_customer_by_id($order['order_customer_id']);
+    $customer_mobile = $customer['customer_mobile1'];
+    $customer_first_name = $customer['customer_first_name'];
+    
+    $SMS->send($customer_mobile, "Sorry ".$customer_first_name.", your Order No.".$order_id." is declined. REASON: ".$reason.". For more details please contact 0712 700 000");
+    
     if($query == true){
         echo true;
     }else{
